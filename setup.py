f73b236f49dfcad1b5048a1964073728d3dfe21f
goujou <goujou@gmx.de>
2016-08-05 22:46:20 +0200

    Changed to README.md in setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # vim:set ff=unix expandtab ts=4 sw=4:
 from setuptools import setup, find_packages
 def readme():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
 setup(name='LAPM',
